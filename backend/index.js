42b3908da8693daa682f315f5714e237216a8a4d
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-05-28 13:30:03 +0100

    updated port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import Taskroute from './routes/tasks.js';
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors())
